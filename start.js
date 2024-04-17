cf231357f6c6363ec94342eabacb0fca122c354c
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-17 12:54:49 -0400

    update

--- start.js
+++ start.js
@@ -2,31 +2,8 @@ module.exports = {
   run: [{
     method: "shell.run",
     params: {
-      id: "npx",
-      message: "npx --yes gepeto@latest",
+      message: "npx --yes gepeto@latest --name {{args.name}} --git {{args.git}} --icon '{{args.icon}}'",
       path: "..",
-      on: [{ "event": "/project name/i", done: true }]
-    }
-  }, {
-    method: "shell.enter",
-    params: {
-      id: "npx",
-      message: "{{args.name}}",
-      on: [{ "event": "/icon url/i", "return": true }]
-    }
-  }, {
-    method: "shell.enter",
-    params: {
-      id: "npx",
-      message: "{{args.icon}}",
-      on: [{ "event": "/3rd party git url/i", "return": true }]
-    }
-  }, {
-    method: "shell.enter",
-    params: {
-      id: "npx",
-      message: "{{args.git}}",
-      on: [{ "event": null, "return": true }]
     }
   }]
 }
