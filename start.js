5e3c723cb569c5d99448fc893a63b766869a7812
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-17 14:15:43 -0400

    update

--- start.js
+++ start.js
@@ -5,5 +5,10 @@ module.exports = {
       message: "npx --yes gepeto@latest --name={{encodeURIComponent(args.name)}} --git={{encodeURIComponent(args.git)}} --icon={{encodeURIComponent(args.icon)}}",
       path: "..",
     }
+  }, {
+    method: "notify",
+    params: {
+      html: "Generation complete. Click the home button to find the generated repository!"
+    }
   }]
 }
