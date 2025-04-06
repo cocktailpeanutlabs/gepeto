ee9d55c88112a6602cf1a006c2e4384a5de94ba1
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-05 17:49:49 -0700

    update

--- start.js
+++ start.js
@@ -10,13 +10,13 @@ module.exports = {
     params: {
       path: "../{{encodeURIComponent(args.name)}}",
       message: [
-        "npx --yes gepeto@latest add link --title=x --value={{args.x || ''}}",
-        "npx --yes gepeto@latest add link --title=github --value={{args.github || ''}}",
-        "npx --yes gepeto@latest add link --title=buymeacoffee --value={{args.buymeacoffee || ''}}",
-        "npx --yes gepeto@latest add link --title=kofi --value={{args.kofi || ''}}",
-        "npx --yes gepeto@latest add link --title=bluesky --value={{args.bsky || ''}}",
-        "npx --yes gepeto@latest add link --title=website --value={{args.website || ''}}",
-        "npx --yes gepeto@latest add link --title=bitcoin --type=bitcoin --value={{args.bitcoin || ''}}"
+        "{{args.github ? 'npx --yes gepeto@latest add link --title=x --value=' + args.x : null}}",
+        "{{args.github ? 'npx --yes gepeto@latest add link --title=github --value=' + args.github : null}}",
+        "{{args.buymeacoffee ? 'npx --yes gepeto@latest add link --title=buymeacoffee --value=' + args.buymeacoffee : null}}",
+        "{{args.kofi ? 'npx --yes gepeto@latest add link --title=kofi --value=' + args.kofi : null}}",
+        "{{args.bsky ? 'npx --yes gepeto@latest add link --title=bluesky --value=' + args.bsky : null}}",
+        "{{args.website ? 'npx --yes gepeto@latest add link --title=website --value=' + args.website : null}}",
+        "{{args.bitcoin ? 'npx --yes gepeto@latest add link --title=bitcoin --type=bitcoin --value=' + args.bitcoin : null}}"
       ]
     }
   }, {
